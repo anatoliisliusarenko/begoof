5962c98aad0ad0cae5fd4f59f27469e927e9cc71
Anatolii Sliusarenko <anatolii.sliusarenko@gmail.com>
2017-01-11 00:18:38 +0200

    Welcome page implemented.

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -9,13 +9,13 @@ use Symfony\Component\HttpFoundation\Request;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/", name="default")
      */
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+            "url" => "http://cs6.pikabu.ru/images/big_size_comm/2015-06_6/1435674692167431188.png"
         ]);
     }
 }
