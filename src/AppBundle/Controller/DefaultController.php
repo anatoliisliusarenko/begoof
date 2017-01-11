f6a2209b31b3fa4ca4482e6b1a2ae8edecb7b4d7
Anatolii Sliusarenko <anatolii.sliusarenko@gmail.com>
2017-01-11 19:50:21 +0200

    Base template structure

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -17,5 +17,8 @@ class DefaultController extends Controller
         return $this->render('default/index.html.twig', [
             "url" => "http://cs6.pikabu.ru/images/big_size_comm/2015-06_6/1435674692167431188.png"
         ]);
+
+        
+
     }
 }
