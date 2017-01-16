d1c8d997d36ef2f60fcda501f5667f7887b50ef2
Anatolii Sliusarenko <anatolii.sliusarenko@gmail.com>
2017-01-16 23:57:02 +0200

    Move templates to the bundle (controllers)

--- src/AppBundle/Controller/AuthController.php
+++ src/AppBundle/Controller/AuthController.php
@@ -6,10 +6,9 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
-class AuthController extends Controller
-{
-    public function loginAction(Request $request)
-    {
-        return $this->render('auth/login.html.twig');
+class AuthController extends Controller {
+	
+    public function loginAction(Request $request) {
+        return $this->render('AppBundle:Auth:login.html.twig');
     }
 }
--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -6,14 +6,12 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
-class DefaultController extends Controller
-{
-    public function indexAction(Request $request)
-    {
+class DefaultController extends Controller {
+    public function indexAction(Request $request) {
         $users = $this->getDoctrine()->getRepository('AppBundle:User')->findAll();
 
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
+        return $this->render('AppBundle:Default:index.html.twig', [
             "users" => $users
         ]);
     }
